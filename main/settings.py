b307d2edfcaa2646b82b0b0d99d0a1824d014ab5
Farkhod95 <misirovfarkhod@gmail.com>
2025-10-14 18:20:46 +0500

    WW

--- main/settings.py
+++ main/settings.py
@@ -85,14 +85,34 @@ ASGI_APPLICATION = 'main.asgi.application'
 
 ### Local Host uchun
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'optivora_db',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST': '127.0.0.1',
+#         'PORT': 3307,
+#         'OPTIONS': {
+#             'charset': 'utf8mb4',
+#             'use_unicode': True,
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+#         },
+#         'CONN_MAX_AGE': 60,
+#         'ATOMIC_REQUESTS': True,
+#     }
+# }
+
+### Server uchun
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'optivora_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': 3306,
+        'NAME': 'host1836067_optivora',
+        'USER': 'host1836067_optivora',
+        'PASSWORD': 'optivora123',
+        'HOST': '127.0.0.1',
+        'PORT': 3307,
         'OPTIONS': {
             'charset': 'utf8mb4',
             'use_unicode': True,
@@ -102,19 +122,6 @@ DATABASES = {
         'ATOMIC_REQUESTS': True,
     }
 }
-
-### Server uchun
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': env("SERVER_DB_NAME"),
-#         'USER': env("SERVER_DB_USER"),
-#         'PASSWORD': env("SERVER_DB_PASSWORD"),
-#         'HOST': env("SERVER_DB_HOST"),
-#         'PORT': env("SERVER_DB_PORT"),
-#     }
-# }
 
 # Password validation
 

1cf9feb29506467cf7d1505e41016a8059956f39
Farkhod95 <misirovfarkhod@gmail.com>
2025-10-15 00:06:26 +0500

    WW

--- passenger_wsgi.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, BASE_DIR)
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()

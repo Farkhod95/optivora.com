a5a464ecca735117703c68e0b978a914b4525968
Farkhod95 <misirovfarkhod@gmail.com>
2025-10-15 16:43:33 +0500

    WWW

--- main/settings.py
+++ main/settings.py
@@ -58,6 +58,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ALLOWED_ORIGINS = [
     "https://optivora-group.com",
+    "http://optivora.universal-uz.uz/",
     "http://localhost:3000",  # agar lokalda ishlayotgan bo‘lsa
     "http://localhost:5173",  # agar lokalda ishlayotgan bo‘lsa
 ]

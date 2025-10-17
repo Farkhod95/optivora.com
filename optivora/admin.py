6a79e1e172c4577ff6f49af961d815c31617a42f
Farkhod95 <misirovfarkhod@gmail.com>
2025-10-17 14:43:58 +0500

    WW

--- optivora/admin.py
+++ optivora/admin.py
@@ -74,7 +74,7 @@ class PartnerAdmin(admin.ModelAdmin):
         'industries', 'equipment_categories', 'order_index',
     )
     search_fields = ('name', 'website', 'description')
-    autocomplete_fields = ('country')
+    autocomplete_fields = ('country',)
 
 
 @admin.register(Project)

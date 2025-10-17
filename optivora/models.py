ef99d6b1f408a93e09a59d792d3068dca54e6614
Farkhod95 <misirovfarkhod@gmail.com>
2025-10-17 10:32:11 +0500

    WW

--- optivora/models.py
+++ optivora/models.py
@@ -331,9 +331,10 @@ class OurWork(BaseModel):
     """Bizning ishlarimiz..."""
     class TYPE(models.TextChoices):
         TYPE1 = 'why_choose ', _('Why Choose Optivora')
-        TYPE2 = 'what_we_do', _('What We Do')
-        TYPE3 = 'our_suppliers', _('Our Suppliers')
-        TYPE4 = 'industries_we_serve', _('Industries We Serve')
+        TYPE2 = 'technical_solution ', _('Technical Solution')
+        TYPE3 = 'what_we_do', _('What We Do')
+        TYPE4 = 'our_suppliers', _('Our Suppliers')
+        TYPE5 = 'industries_we_serve', _('Industries We Serve')
 
     title = models.CharField(_('Xizmat nomi'), null=True, blank=True, max_length=150, unique=True, help_text=_('Xizmatning to‘liq nomi (masalan: Equipment Supply & Procurement)'))  # Unikal nom
     description = models.TextField(_('Batafsil tavsif'), null=True, blank=True, help_text=_('Xizmat tafsilotlari (ixtiyoriy)'))  # Batafsil matn
